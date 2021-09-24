9040649e4dc1079e44760c0ace0a3604873fb7f4
Sadig Karimli <karimli.sadig@gmail.com>
2021-09-25 00:30:12 +0400

    Initial Commit

---
+++ src/test/java/com/etaskify/app/ETaskifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etaskify.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ETaskifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
